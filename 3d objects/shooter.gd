aa5a4baed44d56dbc5c3119ba17154a30b073b4b
ostracized-otter <bennetjcullen@outlook.com>
2026-09-23 11:36:25 +1200

    0.17

--- 3d objects/shooter.gd
+++ 3d objects/shooter.gd
@@ -2,6 +2,6 @@ extends Node3D
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body is RigidBody3D:
-		body.apply_impulse(Vector3(-2,0,0))
+		body.apply_impulse(Vector3(randf_range(-1,-1.5),0,0))
 		print("b")
 	
--- scripts/rigid_body_3d.gd
+++ scripts/rigid_body_3d.gd
@@ -8,3 +8,5 @@ extends RigidBody3D
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body == self:
 		print("g")
+		position = Vector3(1.825, 1.522, 0.006)
+		
